f9c54a04fc951cb3b631fdb01e3dd45b2efc4320
Adam Aidja <adam_w_aidja@progressive.com>
2023-09-19 11:15:43 -0400

    created banking stuff

--- src/CSharpNotesSolution/CSharpNotes/Types/DeclaringLocalVariables.cs
+++ src/CSharpNotesSolution/CSharpNotes/Types/DeclaringLocalVariables.cs
@@ -1,4 +1,42 @@
 ﻿namespace CSharpNotes.Types;
-internal class DeclaringLocalVariables
+public class DeclaringLocalVariables
 {
+    [Fact]
+    public void ExplicitlyTypedLocalVariable()
+    {
+        // Type identifier [= value]
+        int a = 0;
+        int b = 2;
+
+        string dogName = "Rover";
+
+        Assert.Equal("Rover", dogName);
+
+        Assert.Equal(0, a);
+        Assert.Equal(2, b);
+    }
+
+    [Fact]
+    public void ImplicitlyTypedLocalVariablesWithVar()
+    {
+        //if you are going to initialize the variable C# already knows the type 
+        var a = 0;
+        var b = 1.0;
+        var c = "Cat"; //string
+        var d = 'A'; //char
+        var e = true; //bool
+        var salary = 80123.23M;
+
+        var rover = new Dog();
+
+    }
+
+    [Fact]
+    public void ImplicitlyTypedLocalVariablesWithNew()
+    {
+        // .NET 5
+        Dog rover = new();
+    }
 }
+
+public class Dog { }
