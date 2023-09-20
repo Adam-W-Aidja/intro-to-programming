66562ee4e4c4c3a80c52a199e42c0f7d38395d8c
Adam Aidja <adam_w_aidja@progressive.com>
2023-09-20 15:29:50 -0400

    updated

---
+++ src/Week1/stringcalculator-weds/StringCalculator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers == "")
+        {
+            return 0;
+        }
+
+        var numberSplit = numbers.Split(',', '\n');
+        var finalSum = 0;
+
+        foreach (var number in numberSplit)
+        {
+            finalSum += int.Parse(number);
+        }
+
+        return finalSum;
+    }
+}
---
+++ src/Week1/stringcalculator-weds/StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("12", 12)]
+    [InlineData("108", 108)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("108,2", 110)]
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2,3,4,5", 15)]
+    [InlineData("108,2,10,20", 140)]
+    public void UnlimitedNumbers(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,3\n4,5", 13)]
+    [InlineData("100,2\n10\n20", 132)]
+    public void NewLineHandler(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+}
